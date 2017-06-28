067741ef6e3cf71fdccf3647b15aa353bf75dfdd
borilla <github@borilla.co.uk>
2017-06-28 15:01:05 +0800

    Replace with "hand-rolled" markov generator

--- comment-generator.js
+++ comment-generator.js
@@ -1,20 +1,72 @@
-var MarkovGen = require('markov-generator');
+var ngram = require('n-gram');
+
+var N_GRAM_LENGTH = 8;
+var MAX_LENGTH = 1000;
+var TERMINAL_STRING = 'END';
 
 function CommentGenerator() {
-	this.comments = [];
+	this._starts = [];
+	this._ngrams = {};
 }
 
-CommentGenerator.prototype.train = function (comment) {
-	this.comments.push(comment);
+CommentGenerator.prototype.feed = function (comment) {
+	var i, l, ngram, next, arr;
+
+	comment = comment.trim();
+
+	for (i = 0, l = comment.length - N_GRAM_LENGTH; i <= l; ++i) {
+		ngram = comment.slice(i, i + N_GRAM_LENGTH);
+
+		if (i === 0) {
+			this._starts.push(ngram);
+		}
+
+		if (i === l) {
+			next = TERMINAL_STRING;
+		}
+		else {
+			next = comment.slice(i + N_GRAM_LENGTH, i + N_GRAM_LENGTH + 1);
+		}
+
+		arr = this._getNgramArray(ngram);
+		arr.push(next);
+	}
 }
 
 CommentGenerator.prototype.generate = function () {
-	var markov = new MarkovGen({
-		input: this.comments,
-		minLength: 40
-	});
+	var comment = getRandomItem(this._starts);
+	var ngram, arr, next;
 
-	return markov.makeChain();
+	while (comment.length < MAX_LENGTH) {
+		ngram = comment.slice(-N_GRAM_LENGTH);
+		arr = this._getNgramArray(ngram);
+		next = getRandomItem(arr);
+
+		if (next === TERMINAL_STRING) {
+			break;
+		}
+
+		comment += next;
+	}
+
+	return comment;
+}
+
+CommentGenerator.prototype._getNgramArray = function (ngram) {
+	var arr = this._ngrams[ngram];
+
+	if (!arr) {
+		arr = [];
+		this._ngrams[ngram] = arr;
+	}
+
+	return arr;
+}
+
+function getRandomItem(arr) {
+	var index = Math.floor(Math.random() * arr.length);
+
+	return arr[index];
 }
 
 module.exports = CommentGenerator;
--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var fetchYouTubeComments = require('./fetch-youtube-comments');
 var CommentGenerator = require('./comment-generator');
-var videoId = 'ksSZPNQaFP8';
-var MIN_COMMENT_LENGTH = 100;
 
-fetchYouTubeComments(videoId).then(function (comments) {
+var VIDEO_ID = 'ksSZPNQaFP8';
+var MAX_COMMENT_PAGES = 10;
+var MIN_COMMENT_LENGTH = 10;
+var COMMENTS_TO_GENERATE = 10;
+
+fetchYouTubeComments(VIDEO_ID, MAX_COMMENT_PAGES).then(function (comments) {
 	var generator = new CommentGenerator();
 	var i;
 
 	comments.forEach(function (comment) {
 		if (comment.length >= MIN_COMMENT_LENGTH) {
-			generator.train(comment);
+			generator.feed(comment);
 		}
 	});
 
-	for (i = 0; i < 10; ++i) {
-		console.log(generator.generate(), '\n');
+	for (i = 0; i < COMMENTS_TO_GENERATE; ++i) {
+		console.log(generator.generate(), '\n\n--------\n');
 	}
 });
